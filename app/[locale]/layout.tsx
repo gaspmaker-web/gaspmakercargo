6e268819513fe5740037ddcd670709e73aca0261
gaspmaker-web <gaspmaker@gmail.com>
2026-08-21 16:52:33 -0400

    feat: Meta Pixel 1372892784992511 installed

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -75,9 +75,26 @@ const tenantSlug = tenant?.slug || process.env.TENANT_SLUG || 'gaspmaker';
 >
         
         
-        {/* 🔥 QUITA LA ETIQUETA DE ONESIGNAL DE AQUÍ 🔥 */}
+{/* 🔥 META PIXEL 🔥 */}
+<script dangerouslySetInnerHTML={{ __html: `
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1372892784992511');
+  fbq('track', 'PageView');
+`}} />
+<noscript>
+  <img height="1" width="1" style={{display:'none'}}
+    src="https://www.facebook.com/tr?id=1372892784992511&ev=PageView&noscript=1"
+  />
+</noscript>
 
-     <NextIntlClientProvider locale={locale} messages={messages}>
+<NextIntlClientProvider locale={locale} messages={messages}>
   <Providers>
     
   <HeaderWrapper tenantSlug={tenantSlug}>
